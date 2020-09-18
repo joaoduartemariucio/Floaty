b788205f277bdf2b6a34d67d833a3baea2f21b4d
João Vitor Duarte Mariucio <joao.duarte@ftdata.com.br>
2020-09-18 11:46:08 -0300

    SMP Support

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Floaty",
-    platforms: [.iOS(.v10)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Floaty",
             targets: ["Floaty"]),
@@ -18,9 +17,9 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Floaty",
-            path: "Sources"),
+            dependencies: [])
     ]
 )
